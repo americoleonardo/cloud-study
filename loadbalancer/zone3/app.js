af4d721650a0be54ced569df43733d6579797053
Leonardo Americo <leonardoakeisman@gmail.com>
2021-10-16 20:15:35 -0300

    feat: added zone3 test

---
+++ loadbalancer/zone3/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+  return res.status(200).json({"zone":"Zone 3 - OK"});
+})
+
+app.get("/health", (req, res) => {
+  return res.status(200).json({"status": "OK"});
+})
+
+
+module.exports = app;

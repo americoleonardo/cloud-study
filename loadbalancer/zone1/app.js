2c87459fd923e779b54c6fbcf25174a2ce69d2f8
Leonardo Americo <leonardoakeisman@gmail.com>
2021-10-19 21:57:27 -0300

    app

--- loadbalancer/zone1/app.js
+++ loadbalancer/zone1/app.js
@@ -3,13 +3,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
   return res.status(200).json({"zone":"Zone 1 - OK"});
